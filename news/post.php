29a57d1e348d7f5d5bb5b08dfb5ea8f1a67a6be6
mahirk <mahirk@intelfinity.com>
2014-07-10 16:11:51 +0530

    some more pages, added dummy text

--- news/post.php
+++ news/post.php
@@ -6,8 +6,14 @@ $field_message = $_POST['cf_message'];
 $file = 'messages.js';
 $data = json_decode(file_get_contents($file),1);
 $newdata = array('Message'=>$field_firstname, $field_topic, $field_message);
+$data[]= $newdata;
 $put = file_put_contents($file, json_encode($data));
 if($put){
+    $updates = 'updates.js';
+    $parserfeed = fopen("feed.js", "r");
+    $codeer = fread($parserfeed, filesize("feed.js"));
+    $send = file_put_contents($updates, "var a = []; a = ".json_encode($data).$codeer);
+    fclose($parserfeed);
 ?>
 <script language="javascript" type="text/javascript">
 	window.location = 'index.html';
